2f6752a80705991ef00264695ff39ac5c7747aa4
Felipe Yan <71793658+felipeyan@users.noreply.github.com>
2021-06-03 17:58:39 -0300

    Importação dos principais arquivos

---
+++ js/game.js
@@ -0,0 +1,31 @@
+altura = 0;
+largura = 0;
+
+function tamanhoTela() {
+  altura = window.innerHeight;
+  largura = window.innerWidth;
+  // console.log(largura, altura);
+}
+
+function randomPos() {
+  posx = Math.round(Math.random() * largura) - 90;
+  posy = Math.round(Math.random() * altura) - 90;
+
+  posx = posx < 0?0:posx;
+  posy = posy < 0?0:posy;
+  // console.log(posx, posy);
+}
+
+function gerarMosquito() {
+  mosquito = document.createElement('img');
+  mosquito.src = 'imagens/mosca.png';
+  mosquito.className = 'mosq01';
+  mosquito.style.left = posx + 'px';
+  mosquito.style.top = posy + 'px';
+  mosquito.style.position = 'absolute';
+  document.body.appendChild(mosquito);
+}
+
+tamanhoTela();
+
+document.addEventListener("DOMContentLoaded", gerarMosquito);
